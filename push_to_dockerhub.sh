4e9c859b4d832f73206b69ca22d4faec7e435cac
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 18:21:34 +0100

    front end and back end paths cd into

--- push_to_dockerhub.sh
+++ push_to_dockerhub.sh
@@ -3,7 +3,10 @@ ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 
 
 cat ~/docker_password.txt | sudo docker login --username manishreddy1 --password-stdin
 
+cd ~/CNE-SFIA-2/frontend
 sudo docker push manishreddy1/cne_front_end:latest
+
+cd ~/CNE-SFIA-2/backend
 sudo docker push manishreddy1/backend:latest
 
 EOF
