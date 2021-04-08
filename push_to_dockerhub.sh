084ba240cc9619c66d9a5343665f7c4835cdde47
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 18:42:43 +0100

    docker tag error correct

--- push_to_dockerhub.sh
+++ push_to_dockerhub.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
+sudo docker logout
 cat ~/docker_password.txt | sudo docker login --username manishreddy1 --password-stdin
 
 sudo docker tag cne_flask_frontend:latest manishreddy1/cne_flask_frontend:latest
-sudo docker tag cne_flask_backend:latest manishreddy1/backend:latest
+sudo docker tag backend:latest manishreddy1/backend:latest
 
 sudo docker push manishreddy1/cne_flask_frontend:latest
 sudo docker push manishreddy1/backend:latest
-
 
 EOF
