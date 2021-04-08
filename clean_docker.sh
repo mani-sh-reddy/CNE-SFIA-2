4f1b4bf6be98d995532764912dd183336e9e97b2
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 16:31:01 +0100

    joined

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
-sudo docker stop $(sudo docker ps -aq)
-sudo docker system prune --all --force
+sudo docker stop $(sudo docker ps -aq) || sudo docker system prune --all --force
 
 EOF
