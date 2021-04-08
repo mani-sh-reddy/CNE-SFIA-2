a709d7ead5caf2217b0e8a04b71f50174581118c
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 15:49:19 +0100

    changed docker run path

--- run_docker_images.sh
+++ run_docker_images.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
+cd ~/CNE-SFIA-2/frontend
+
 sudo docker run -d -p 5000:5000 --name frontend frontend
+
+cd ~/CNE-SFIA-2/backend
 
 sudo sh ~/docker_run_script.sh
 
