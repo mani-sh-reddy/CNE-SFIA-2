e24d17f873adc4159156f3a275319d6db6a86cd6
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 18:16:44 +0100

    docker push changed to utilise the password file on host.

--- push_to_dockerhub.sh
+++ push_to_dockerhub.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
-sudo sh ~/push_to_docker_hub.sh
+cat ~/docker_password.txt | sudo docker login --username manishreddy1 --password-stdin
+
+sudo docker push manishreddy1/cne_front_end:latest
+sudo docker push manishreddy1/backend:latest
 
 EOF
