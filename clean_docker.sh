4faacce6231bbc8ac2fa56bc4beb120c2d21d7c4
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 16:38:25 +0100

    docker rm changed

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
-sudo docker stop $(sudo docker ps -aq) || sudo docker system prune --all --force
+sudo docker stop cne_flask_backend cne_flask_frontend
+sudo docker rmi -f cne_flask_backend:latest cne_flask_frontend:latest 
 
 EOF
