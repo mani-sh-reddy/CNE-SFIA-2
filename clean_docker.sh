173ce0b220bc91d5eb7e1c2508185064e21f5a19
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 17:54:53 +0100

    add docker restart

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
+sudo service docker stop
+sudo rm /var/lib/docker/network/files/local-kv.db
+sudo service docker start
+
 sudo docker stop backend cne_flask_frontend
 sudo docker rm backend cne_flask_frontend
 sudo docker rmi -f backend:latest cne_flask_frontend:latest 
