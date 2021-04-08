b4797ee82415024c44e2cf6204ec1b750b2c4f7c
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 15:54:57 +0100

    chagned image name

--- run_docker_images.sh
+++ run_docker_images.sh
@@ -3,7 +3,7 @@ ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 
 
 cd ~/CNE-SFIA-2/frontend
 
-sudo docker run -d -p 5000:5000 --name frontend frontend
+sudo docker run -d -p 5000:5000 --name cne_flask_frontend cne_flask_frontend
 
 cd ~/CNE-SFIA-2/backend
 
