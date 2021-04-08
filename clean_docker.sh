ff5bd64e0b3705b7b295f41237ffacca6f067675
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-08 16:10:39 +0100

    auto confirmation with -y

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
-docker stop $(docker ps -aq) # stop all containers
-docker system prune --all
+docker stop $(docker ps -aq) -y
+docker system prune --all -y
 
-docker rm --force docker ps -qa || docker rmi --force docker images -qa
+docker rm --force docker ps -qa -y || docker rmi --force docker images -qa -y
 
 EOF
