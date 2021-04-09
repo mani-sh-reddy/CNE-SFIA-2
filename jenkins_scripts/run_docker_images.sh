09b6134eb90007fa5262a4953654f4938a3b4f9a
Manish <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-09 05:45:28 +0000

    ?

--- jenkins_scripts/run_docker_images.sh
+++ jenkins_scripts/run_docker_images.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.132.2.39 << EOF
 
-sudo docker network create new-net
+sudo docker network create my-network
 
 cd ~/CNE-SFIA-2/frontend
 
-sudo docker run --network new-net -d -p 5000:5000 --name cne_flask_frontend cne_flask_frontend
+sudo docker run --network my-network -d -p 5000:5000 --name cne_flask_frontend cne_flask_frontend
 
 cd ~/CNE-SFIA-2/backend
 
